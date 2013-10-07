1774a11a150febe15d6a881e76a373fdb781cce9
idsc3101 <idsc3101@gmail.com>
2013-10-07 15:08:07 -0500

    solved 1 and 2

--- RightTriangle/RightTriangle/Form1.vb
+++ RightTriangle/RightTriangle/Form1.vb
@@ -3,9 +3,25 @@ Public Class Form1
 
 
     Private Sub btnCalculate_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculate.Click
+        Dim one As String
+        Dim other As String
+        one = txtSideOne.Text
+        other = txtSideTwo.Text
 
-        txtHyp.Text = "Not Implemented Yet"
+
+        txtHyp.Text = TriCalc(one, other)
 
     End Sub
 
+    Function TriCalc(ByVal one As String, ByVal other As String) As String
+        Dim oneNum As Double
+        Dim otherNum As Double
+        Dim hyp As Double
+        Dim hypStr As String
+        oneNum = CDbl(one)
+        otherNum = CDbl(other)
+        hyp = Math.Sqrt(oneNum ^ 2 + otherNum ^ 2)
+        hypStr = CStr(hyp)
+        Return hypStr
+    End Function
 End Class
